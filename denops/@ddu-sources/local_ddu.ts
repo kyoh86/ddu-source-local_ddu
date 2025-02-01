d88f20293416f0625cdec7bce590d06f82c4eab9
kyoh86 <me@kyoh86.dev>
2025-02-01 23:07:26 +0900

    Update dependencies

--- denops/@ddu-sources/local_ddu.ts
+++ denops/@ddu-sources/local_ddu.ts
@@ -1,11 +1,11 @@
-import type { GatherArguments } from "jsr:@shougo/ddu-vim@~9.4.0/source";
+import type { GatherArguments } from "jsr:@shougo/ddu-vim@~9.5.0/source";
 import {
   ActionFlags,
   type Actions,
   type DduItem,
   type Item,
-} from "jsr:@shougo/ddu-vim@~9.4.0/types";
-import { BaseSource } from "jsr:@shougo/ddu-vim@~9.4.0/source";
+} from "jsr:@shougo/ddu-vim@~9.5.0/types";
+import { BaseSource } from "jsr:@shougo/ddu-vim@~9.5.0/source";
 import type { Denops } from "jsr:@denops/std@~7.4.0";
 
 type ActionData = { name: string };
